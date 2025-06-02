d3729cedaba645ec0ce7bd21350f511379f687e2
thodoris-papadopoulos <theodorepapadopoulos2002@gmail.com>
2025-06-02 23:24:17 +0300

    Update models.py

--- models.py
+++ models.py
@@ -117,12 +117,12 @@ class Customer:
             
 
 class Appointment:
-    def __init__(self, customer_id, date, time, services, duration=20, notes="", id=None):
+    def __init__(self, customer_id, date, time, services, duration=20, notes="", id=None)
         self.customer_id = customer_id
         self.date = date
         self.time = time
         self.services = services
-        self.duration = duration
+        self.duration = int(duration)
         self.notes = notes
         self.id = id
         self.date_time = datetime.strptime(f"{self.date} {self.time}", "%d-%m-%Y %H:%M")
