766f63b29a2493c8374c695e3c29723c869b13d1
thodoris-papadopoulos <theodorepapadopoulos2002@gmail.com>
2025-06-11 16:12:37 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,35 +1,5 @@
-# Classes
+
 import sqlite3
-# from datetime import datetime, timedelta
-
-# Συνάρτηση ρύθμισης της βάσης δεδομένων 
-def setup_database():
-    # Συνδέεται με τη βάση δεδομένων SQLite ή τη δημιουργεί εάν δεν υπάρχει
-    conn = sqlite3.connect('salon_appointments.db')
-    c = conn.cursor()
-    
-    # Δημιουργία του πίνακα για πελάτες αν δεν υπάρχει ήδη
-    c.execute('''CREATE TABLE IF NOT EXISTS customers (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,  -- Πρωτεύων κλειδί για μοναδική ταυτοποίηση
-                    first_name TEXT NOT NULL,  -- Όνομα πελάτη
-                    last_name TEXT NOT NULL,   -- Επίθετο πελάτη
-                    phone TEXT NOT NULL UNIQUE, -- Τηλέφωνο, μοναδικό για κάθε πελάτη
-                    email TEXT NOT NULL UNIQUE  -- Email, μοναδικό για κάθε πελάτη
-                 )''')
-
-    # Δημιουργία του πίνακα για ραντεβού αν δεν υπάρχει ήδη
-    c.execute('''CREATE TABLE IF NOT EXISTS appointments (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,  -- Πρωτεύων κλειδί για μοναδική ταυτοποίηση
-                    customer_id INTEGER NOT NULL,     -- Αναφορά στο ID του πελάτη    
-                    datetime TEXT NOT NULL,         -- Ημερομηνία και ώρα του ραντεβού
-                    services TEXT NOT NULL,
-                    duration INTEGER NOT NULL DEFAULT 20,        -- Διάρκεια ραντεβού σε λεπτά
-                    notes TEXT,
-                    FOREIGN KEY (customer_id) REFERENCES customers (id)  -- Σχέση με τον πίνακα πελατών
-                 )''')
-    conn.commit()
-    conn.close()
-    
 
 class Customer:
     def __init__(self, first_name, last_name, phone, email, id=None):
@@ -188,7 +158,7 @@ class Appointment:
         
     @staticmethod
     def get_by_date(date):
-        """Επιστρέφει όλα τα ραντεβού μιας συγκεκριμένης ημέρας"""
+        #Επιστρέφει όλα τα ραντεβού μιας συγκεκριμένης ημέρας
         with sqlite3.connect('salon_appointments.db') as conn:
             c = conn.cursor()
             
